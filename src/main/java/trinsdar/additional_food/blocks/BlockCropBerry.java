5d23c42aa59766e076419aab6dfbcf5e08b4aadd
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-10-07 18:43:30 -0400

    got drops and middle click item working for berry bush

--- src/main/java/trinsdar/additional_food/blocks/BlockCropBerry.java
+++ src/main/java/trinsdar/additional_food/blocks/BlockCropBerry.java
@@ -19,17 +19,36 @@ import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.registries.ForgeRegistries;
 import trinsdar.additional_food.AdditionalFood;
+import trinsdar.additional_food.Registry;
 
 public class BlockCropBerry extends SweetBerryBushBlock {
     Item berry;
     public BlockCropBerry(String berry) {
         super(Properties.create(Material.PLANTS).tickRandomly().doesNotBlockMovement().sound(SoundType.SWEET_BERRY_BUSH));
-        this.berry = ForgeRegistries.ITEMS.getValue(new ResourceLocation(AdditionalFood.MODID, berry));
     }
 
     @Override
     public ItemStack getItem(IBlockReader worldIn, BlockPos pos, BlockState state) {
-        return new ItemStack(berry);
+        return new ItemStack(getItem());
+    }
+
+    public Item getItem() {
+        if (this == Registry.BLUEBERRY_BUSH.get()){
+            return Registry.BLUEBERRY.get();
+        }
+        if (this == Registry.BLACKBERRY_BUSH.get()){
+            return Registry.BLACKBERRY.get();
+        }
+        if (this == Registry.GOOSEBERRY_BUSH.get()){
+            return Registry.GOOSEBERRY.get();
+        }
+        if (this == Registry.RASPBERRY_BUSH.get()){
+            return Registry.RASPBERRY.get();
+        }
+        if (this == Registry.STRAWBERRY_BUSH.get()){
+            return Registry.STRAWBERRY.get();
+        }
+        return Items.SWEET_BERRIES;
     }
 
     @Override
@@ -40,7 +59,7 @@ public class BlockCropBerry extends SweetBerryBushBlock {
             return false;
         } else if (i > 1) {
             int j = 1 + worldIn.rand.nextInt(2);
-            spawnAsEntity(worldIn, pos, new ItemStack(berry, j + (flag ? 1 : 0)));
+            spawnAsEntity(worldIn, pos, new ItemStack(getItem(), j + (flag ? 1 : 0)));
             worldIn.playSound((PlayerEntity)null, pos, SoundEvents.ITEM_SWEET_BERRIES_PICK_FROM_BUSH, SoundCategory.BLOCKS, 1.0F, 0.8F + worldIn.rand.nextFloat() * 0.4F);
             worldIn.setBlockState(pos, state.with(AGE, 1), 2);
             return true;
