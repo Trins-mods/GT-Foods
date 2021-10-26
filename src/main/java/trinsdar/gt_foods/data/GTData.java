e848b67b7ce78ba33a742afed8df63c5fb328b7f
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-10-26 00:20:50 -0400

    started work on recipes

---
+++ src/main/java/trinsdar/gt_foods/data/GTData.java
@@ -0,0 +1,25 @@
+package trinsdar.gt_foods.data;
+
+import muramasa.antimatter.AntimatterAPI;
+import muramasa.antimatter.Data;
+import muramasa.antimatter.Ref;
+import muramasa.antimatter.item.ItemBasic;
+import muramasa.antimatter.material.Material;
+import muramasa.antimatter.material.MaterialType;
+import muramasa.antimatter.material.TextureSet;
+import trinsdar.gt_foods.GTFoods;
+
+public class GTData {
+
+    public static final Material StainlessSteel = AntimatterAPI.register(Material.class, new Material(Ref.ID, "stainless_steel", 0xc8c8dc, TextureSet.NONE)).flags(Data.RING, Data.ROD, Data.PLATE_TINY);
+
+    public static final ItemBasic<?> SlicerBladeFrame = new ItemBasic<>(GTFoods.MODID, "slicer_blade_frame");
+    public static final ItemBasic<?> FlatSlicerBlades = new ItemBasic<>(GTFoods.MODID, "flat_slicer_blades");
+    public static final ItemBasic<?> GridSlicerBlades = new ItemBasic<>(GTFoods.MODID, "grid_slicer_blades");
+    public static final ItemBasic<?> EigthsSlicerBlades = new ItemBasic<>(GTFoods.MODID, "eigths_slicer_blades");
+    public static final ItemBasic<?> SplitSlicerBlades = new ItemBasic<>(GTFoods.MODID, "split_slicer_blades");
+    public static final ItemBasic<?> HollowQuartersSlicerBlades = new ItemBasic<>(GTFoods.MODID, "hollow_quarters_slicer_blades");
+
+    public static void init(){
+    }
+}
---
+++ src/main/java/trinsdar/gt_foods/loader/SlicerLoader.java
@@ -0,0 +1,35 @@
+package trinsdar.gt_foods.loader;
+
+import muramasa.antimatter.recipe.ingredient.RecipeIngredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import trinsdar.gt_foods.data.Data;
+import trinsdar.gt_foods.data.GTData;
+import trinsdar.gt_foods.data.RecipeMaps;
+
+public class SlicerLoader {
+
+    public static void init(){
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.LEMON, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.LEMON_SLICE, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.TOMATO, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.TOMATO_SLICE, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.ONION, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.ONION_SLICE, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.CUCUMBER, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.CUCUMBER_SLICE, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.PICKEL, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.PICKEL_SLICE, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Items.CARROT, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.CARROT_SLICE, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.BANANA, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.BANANA_SLICE, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Items.APPLE, 1), RecipeIngredient.of(GTData.HollowQuartersSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.APPLE_SLICE, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.PINEAPPLE, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.PINEAPPLE_SLICE, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.CHEESE, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.CHEESE_SLICE, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.RAW_HAM, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.RAW_HAM_SLICE, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.COOKED_HAM, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.COOKED_HAM_SLICE, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.BUN, 1), RecipeIngredient.of(GTData.SplitSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.SLICED_BUN, 2)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Items.BREAD, 1), RecipeIngredient.of(GTData.SplitSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.SLICED_BREAD, 2)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.BAGUETTE, 1), RecipeIngredient.of(GTData.SplitSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.SLICED_BAGUETTE, 2)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Items.POTATO, 1), RecipeIngredient.of(GTData.GridSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.RAW_FRIES)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Items.POTATO, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.RAW_POTATO_CHIPS)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.CHOCOLATE_DOUGH, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.COOKIE_SHAPED_DOUGH, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.SUGARY_RAISIN_DOUGH, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.COOKIE_SHAPED_RAISIN_DOUGH, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Data.SUGARY_CHOCOLATE_RAISIN_DOUGH, 1), RecipeIngredient.of(GTData.FlatSlicerBlades, 1).setNoConsume()).io(new ItemStack(Data.COOKIE_SHAPED_CHOCOLATE_RAISIN_DOUGH, 4)).add(16, 16);
+        RecipeMaps.SLICING.RB().ii(RecipeIngredient.of(Items.MELON, 1), RecipeIngredient.of(GTData.EigthsSlicerBlades, 1).setNoConsume()).io(new ItemStack(Items.MELON_SLICE, 8), new ItemStack(Items.MELON_SEEDS)).add(16, 16);
+    }
+}
