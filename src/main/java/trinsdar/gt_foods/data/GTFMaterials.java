61925e0c917bbbbbb74f6dfa6ca5abbd86ed42e5
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-12-02 23:53:48 -0500

    added more juicer recipes and more materials

--- src/main/java/trinsdar/gt_foods/data/GTFMaterials.java
+++ src/main/java/trinsdar/gt_foods/data/GTFMaterials.java
@@ -19,14 +19,23 @@ public class GTFMaterials {
     public static final Material AppleCiderVinegar = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "apple_cider_vinegar", 0xa17a00, NONE)).asFluid();
     public static final Material Mayonnaise = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "mayonnaise", 0xd0c49e, NONE)).asFluid();
     public static final Material BarbecueSauce = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "barbecue_sauce", 0x520000, NONE)).asFluid();
+    public static final Material ChiliSauce = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "chili_sauce", 0xb50000, NONE)).asFluid();
     public static final Material Ketchup = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "ketchup", 0x950000, NONE)).asFluid();
     public static final Material FishOil = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "fish_oil", 0xffc400, NONE)).asFluid(6);
     public static final Material SeedOil = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "seed_oil", 0xc4ff00, NONE)).asFluid(6);
+    public static final Material NutOil = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "nut_oil", 0xeaaa40, NONE)).asFluid();
     public static final Material HotFryingOil = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "hot_frying_oil", 0xd0aa42, NONE)).asFluid();
 
     public static final Material Chocolate = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "chocolate", 0x643200, NONE)).asDust(GROUND).asFluid();
     public static final Material BlackPepper = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "black_pepper", 0x282828, NONE)).asDust(GROUND);
     public static final Material Cinnamon = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "cinnamon", 0x7a5335, NONE)).asDust(GROUND);
+    public static final Material Cocoa = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "cocoa", 0xbe5f00, NONE)).asDust(GROUND);
+    public static final Material Coffee = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "coffee", 0x964b00, NONE)).asDust(GROUND);
+    public static final Material Chili = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "chili", 0xc80000, NONE)).asDust(GROUND);
+    public static final Material Nutmeg = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "nutmeg", 0xf0dcb4, NONE).asDust(GROUND));
+    public static final Material Peanut = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "peanut", 0xf0d2a0, NONE).asDust(GROUND));
+    public static final Material Hazelnut = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "hazelnut", 0xf0c88c, NONE).asDust(GROUND));
+    public static final Material FishMeal = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "fishmeal", 0xff9664, NONE).asDust(GROUND));
 
     public static final Material Wheat = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "wheat", 0xffffc4, NONE)).flags(FLOUR);
     public static final Material Barley = AntimatterAPI.register(Material.class, new Material(GTFoods.MODID, "barley", 0xc4ffc4, NONE)).flags(FLOUR);
--- src/main/java/trinsdar/gt_foods/loader/JuicerLoader.java
+++ src/main/java/trinsdar/gt_foods/loader/JuicerLoader.java
@@ -1,5 +1,6 @@
 package trinsdar.gt_foods.loader;
 
+import muramasa.antimatter.Data;
 import muramasa.antimatter.recipe.ingredient.RecipeIngredient;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -12,8 +13,14 @@ import trinsdar.gt_foods.data.RecipeMaps;
 public class JuicerLoader {
 
     public static void init(){
-        //RecipeMaps.JUICING.RB().ii(RecipeIngredient.of(Items.COCOA_BEANS, 1)).io(new ItemStack(GTFMaterials.Cocoa))
+        RecipeMaps.JUICING.RB().ii(RecipeIngredient.of(Items.COCOA_BEANS, 1)).io(Data.DUST.get(GTFMaterials.Cocoa, 1)).fo(GTFMaterials.SeedOil.getLiquid(75)).add();
+        RecipeMaps.JUICING.RB().ii(RecipeIngredient.of(GTFData.CHILI_PEPPER, 1)).io(Data.DUST.get(GTFMaterials.Chili, 1)).fo(GTFMaterials.ChiliSauce.getLiquid(75)).add();
         RecipeMaps.JUICING.RB().ii(RecipeIngredient.of(Tags.Items.SEEDS, 1)).fo(GTFMaterials.SeedOil.getLiquid(50)).add();
         RecipeMaps.JUICING.RB().ii(RecipeIngredient.of(Items.EGG, 1)).io(new ItemStack(GTFData.EGG_WHITE), new ItemStack(GTFData.EGG_YOLK)).add();
+        RecipeMaps.JUICING.RB().ii(RecipeIngredient.of(Items.COD, 1)).io(Data.DUST.get(GTFMaterials.FishMeal, 1)).fo(GTFMaterials.FishOil.getLiquid(1000)).add();
+        RecipeMaps.JUICING.RB().ii(RecipeIngredient.of(Items.SALMON, 1)).io(Data.DUST.get(GTFMaterials.FishMeal, 1)).fo(GTFMaterials.FishOil.getLiquid(2000)).add();
+        RecipeMaps.JUICING.RB().ii(RecipeIngredient.of(Items.TROPICAL_FISH, 1)).io(Data.DUST.get(GTFMaterials.FishMeal, 1)).fo(GTFMaterials.FishOil.getLiquid(500)).add();
+        RecipeMaps.JUICING.RB().ii(RecipeIngredient.of(GTFData.HAZELNUT, 1)).io(Data.DUST.get(GTFMaterials.Hazelnut, 1)).fo(GTFMaterials.NutOil.getLiquid(75)).add();
+        RecipeMaps.JUICING.RB().ii(RecipeIngredient.of(GTFData.PEANUT, 1)).io(Data.DUST.get(GTFMaterials.Peanut, 1)).fo(GTFMaterials.NutOil.getLiquid(75)).add();
     }
 }
